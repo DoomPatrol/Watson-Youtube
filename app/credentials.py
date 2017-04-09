b3347acbd90232a8fb82b8bb4a15195022b432ae
Austin Grandt <austin.grandt@gmail.com>
2017-04-09 15:33:20 -0500

    added credentials for real

---
+++ app/credentials.py
@@ -0,0 +1,8 @@
+watson_credentials = {
+'password': '0oJI0cvn5Mcz',
+'username': '29f0d6ed-697a-4817-a71c-bcaedd4a4dae'
+}
+
+youtube_credentials = {
+'api_key': 'AIzaSyBEixt56vbvLOYrw1D2nVKQO2s-ytLUe84'
+}
